70b05597432747b56e13d22364510800b347ea8c
erdemelvan <erdemelvan35@gmail.com>
2023-10-14 06:00:43 +0300

    feat: login and register buttons added on landing page, after sign out url added on userbutton

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
     <div>
       <p>dashboard page (protected)</p>
+      <UserButton afterSignOutUrl='/' />
     </div>
   );
 };
